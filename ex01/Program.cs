38a4b9f1f5f85fc9d26f354010a9c51e69988079
AndreySh <shingaryovan@gmail.com>
2023-02-09 23:51:27 +1200

    пример работы метода Count и правильного кода по работе с массивом

---
+++ ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+void PrintArray(int[] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        Console.Write($" {arg[i]} ");
+    }
+}
+
+void FillArray(int[] arg, int min, int max)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        arg[i] = new Random().Next(min,max);
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int n = ReadInt("Введите количество элементов массива: ");
+int minimum = ReadInt("Введите значение минимального элемента: ");
+int maximum = ReadInt("Введите значение максимального элемента: ");
+
+int[] array = new int[n];
+
+FillArray(array, minimum, maximum);
+
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine($"Ammount of even element: {array.Count(number => number % 2 == 0)}");    //выводит количество четных элементов
+Console.WriteLine($"Ammount of odd element: {array.Count(number => number % 2 != 0)}");     //выводит количество нечетных элементов
+
