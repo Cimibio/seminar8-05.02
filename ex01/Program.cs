b9e2f027221618eb8f5efd7ae73af8af252c429e
AndreySh <shingaryovan@gmail.com>
2023-02-10 00:41:59 +1200

    правильный код работы с массивом, примеры использования методов Count, Where, Select

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -36,3 +36,9 @@ Console.WriteLine();
 Console.WriteLine($"Ammount of even element: {array.Count(number => number % 2 == 0)}");    //выводит количество четных элементов
 Console.WriteLine($"Ammount of odd element: {array.Count(number => number % 2 != 0)}");     //выводит количество нечетных элементов
 
+int[] oddElements = array.Where(x => x % 2 != 0).ToArray();         //вернет все нечетные элементы в новый массив oddElements
+PrintArray(oddElements);
+Console.WriteLine();
+
+int[] NegateArray = array.Select(x => -x).ToArray();                //вернет все элементы массива на противоположные
+PrintArray(NegateArray);
